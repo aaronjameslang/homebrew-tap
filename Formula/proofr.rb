711f9574ebe0b811d9dff448400e1283dbc5d6e8
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-04-04 14:51:52 +0100

    Update hash

--- Formula/proofr.rb
+++ Formula/proofr.rb
@@ -2,7 +2,7 @@ class Proofr < Formula
   desc "Git hook to check conformance to http://chris.beams.io/posts/git-commit/"
   homepage "https://github.com/aaronjameslang/proofr"
   url "https://github.com/aaronjameslang/proofr/archive/2.1.0.tar.gz"
-  sha256 "9903ed0524c358e940c85fbb56877b1c0f1656f23d0e0d3cdaa1bbc58cfdef4c"
+  sha256 "c1d9693a25abcac17d4e86a3e55c8d4c453cdfc221e0cff347b8715f9b310a75"
 
   def install
     cp_r "./.", prefix.to_s
